c7cea88ecfe6f50ff243a590afc4d5457adb115f
Juan Castillo <juan.castillo@sienz.com>
2021-08-21 09:35:12 -0400

    factorizado de codigo usando bloques en plantillas y validaciones de formularios

--- tv_shows/models.py
+++ tv_shows/models.py
@@ -1,10 +1,43 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import date,datetime
 
+# TODO Añadiendo validaciones
+# Nuestro administrador personalizado!
+# Ningún método en nuestro nuevo administrador debería recibir el objeto de solicitud completo como argumento!
+# (solo partes, como request.POST)
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # agregue claves y valores al diccionario de errores para cada campo no válido
+        # if postData['title_show'] in :
+        #     errors["title_show"] = "El SHOW ya existe"
+
+        hoy = date.today()
+        fecha_formulario = datetime.strptime(postData['date_show'], "%Y-%m-%d").date()
+
+        if fecha_formulario >= hoy:
+            errors["date_show"] = "La fecha debe ser menor al dia de hoy"
+
+        # if postData['date_show'] == str(date.today()):
+        #     errors["date_show"] = "La fecha debe ser distinta al dia de hoy"
+
+        if len(postData['title_show']) < 2:
+            errors["title_show"] = "El TITULO del show debe tener al menos 2 caracteres"
+        if len(postData['network_show']) < 3:
+            errors["network_show"] = "Network debe tener al menos 3 caracteres"
+        if len(postData['desc_show']) >0:
+            if len(postData['desc_show']) <10:
+                errors["desc_show"] = "La DESCRIPCION debe tener al menos 10 caracteres"
+        return errors
+
 class Show(models.Model):
-    title = models.CharField(max_length=45)
+    title = models.CharField(max_length=45, unique=True)
     network = models.CharField(max_length=45)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True) #hacemos datos opcional
     release_date = models.DateField(default='1999-01-01')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager() #se añade esto para la validacion
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from time import  localtime, strftime
 from tv_shows.models import *
+from django.contrib import messages #para mensajes de validaciones
+
 
 
 def inicio(request):
@@ -15,25 +17,46 @@ def index(request):
     if request.method == 'POST':
         print(request.POST)
 
-        # leemos las variables del formulario
+        errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en él
+        if len(errors) > 0:
+            # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+            for key, value in errors.items():
+                messages.error(request, value)
 
-        title_show= request.POST['title_show']
-        network_show= request.POST['network_show']
-        date_show= request.POST['date_show']
-        desc_show= request.POST['desc_show']
+            request.session['show_title']=request.POST['title_show']
+            request.session['network_show']=request.POST['network_show']
+            request.session['desc_show']=request.POST['desc_show']
+            request.session['date_show']=request.POST['date_show']
+            # redirigir al usuario al formulario para corregir los errores
+            return redirect('/shows/new')
         
-        
-        # ejecutamos comando ORM para crear usauario, los campos deben coincidir con el modelo definido
-        show_db = Show.objects.create(
-            title = title_show,
-            network = network_show,
-            description = desc_show,
-            release_date= date_show,
-            )
-        
-        print("Show CREADO CORRECTAMENTEE!!!", show_db)
-        
-    return redirect(f'/shows/{show_db.id}')
+        else:
+            # si el objeto de errores está vacío, eso significa que no hubo errores.
+            # recuperar el blog para actualizarlo, realizar los cambios y guardar
+            # leemos las variables del formulario
+            request.session['show_title']=""
+            request.session['network_show']=""
+            request.session['desc_show']=""
+            request.session['date_show']=""
+
+
+            title_show= request.POST['title_show']
+            network_show= request.POST['network_show']
+            date_show= request.POST['date_show']
+            desc_show= request.POST['desc_show']
+                        
+            # ejecutamos comando ORM para crear usauario, los campos deben coincidir con el modelo definido
+            show_db = Show.objects.create(
+                title = title_show,
+                network = network_show,
+                description = desc_show,
+                release_date= date_show,
+                )
+            messages.success(request, f"Show creado correctamente el show {title_show}")
+            print("Show CREADO CORRECTAMENTEE!!!", show_db)
+            
+            return redirect(f'/shows/{show_db.id}')
 
 #FUNCION MUESTRA TODOS LOS SHOWS
 def shows(request):
@@ -75,25 +98,37 @@ def edit_show(request,val):
 def update_show(request,val):
         print(request.POST)
 
-        # leemos las variables del formulario
-
-        title_show= request.POST['title_show']
-        network_show= request.POST['network_show']
-        date_show= request.POST['date_show']
-        desc_show= request.POST['desc_show']
+        errors = Show.objects.basic_validator(request.POST)
+        # compruebe si el diccionario de errores tiene algo en él
+        if len(errors) > 0:
+            # si el diccionario de errores contiene algo, recorra cada par clave-valor y cree un mensaje flash
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirigir al usuario al formulario para corregir los errores
+            return redirect(f'/shows/{val}/edit')
         
-        
-        # ejecutamos comando ORM para actualizar tv show, los campos deben coincidir con el modelo definido
-        tvupdate= Show.objects.get(id =val)
-        tvupdate.title=title_show
-        tvupdate.network=network_show
-        tvupdate.release_date=date_show
-        tvupdate.description=desc_show
-        tvupdate.save()
-        
-        print(f"Show {tvupdate.title} ACTUALIZADO CORRECTAMENTEE!!!")
-        
-        return redirect(f'/shows/{tvupdate.id}')
+        else:
+            # si el objeto de errores está vacío, eso significa que no hubo errores.
+            # recuperar el blog para actualizarlo, realizar los cambios y guardar
+            # leemos las variables del formulario
+            title_show= request.POST['title_show']
+            network_show= request.POST['network_show']
+            date_show= request.POST['date_show']
+            desc_show= request.POST['desc_show']
+            
+            # ejecutamos comando ORM para actualizar tv show, los campos deben coincidir con el modelo definido
+            tvupdate= Show.objects.get(id =val)
+            
+            tvupdate.title=title_show
+            tvupdate.network=network_show
+            tvupdate.release_date=date_show
+            tvupdate.description=desc_show
+            tvupdate.save()
+            messages.success(request, F"Show {tvupdate.title} actualizado correctamente")
+            
+            print(f"Show {tvupdate.title} ACTUALIZADO CORRECTAMENTEE!!!")
+            # redirigir a la ruta de exito
+            return redirect(f'/shows/{tvupdate.id}')
 
 #FUNCION BORRAR SHOW
 def delete_show(request,val):
